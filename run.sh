6f1f3243235cf8615bedc87e8ce808d58b5203d8
junkim1310 <107043881+junkim1310@users.noreply.github.com>
2025-03-22 10:14:40 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 CUDA_VISIBLE_DEVICES=0 python test_demo.py \
     --data_dir [path to your data dir] \
     --save_dir [path to your save dir] \
-    --model_id 0
+    --model_id 43
 
 
 # --- When only LSDIR_DIV2K_test datasets are included (For Organizer) ---
